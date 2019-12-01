829e4fad86d9481ebe6fa74c9d5fd73eb646d6a6
buckmiller <par23mar5@gmail.com>
2019-12-02 01:36:35 +0400

    Added minor changes

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,18 +1,34 @@
-window.onload = function () {
-        document.addEventListener('DOMContentLoaded', function () {
-                if ()
-                        document.querySelector('.switch').textContent = "+";
-                else
-                        document.querySelector('.switch').textContent = "-";
-        });
+function collapse (element) {
+    const collapse = document.getElementById(element);
+    const link = document.getElementsByClassName(element);
+    if (!collapse.classList.contains('in')) {
+        link[0].innerHTML = "-";
+        return;
+    }
+    link[0].innerHTML = "+";
+}
 
-
-
-        // function changeBg(divObj){
-//         divObj.style.background="white";
+// function changeBg(class) {
+//     $('class').css('background-color', 'white');
 // }
 
 
+// function changeBg(block) {
+//     const color = document.getElementsByClassName(block);
+//     block.onclick = function(event) {
+//         let target = event.target; // где был клик?
+//
+//         if (target.tagName = block) return; // не на block? тогда не интересует
+//
+//          // подсветить block
+//     };
+// };
+//
+//
+// // function color(block){
+// //
+// // }
+//
 
 
 
