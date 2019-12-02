c6a8587313b4994bc5a91d4b8dfd57ed90c8ee6e
buckmiller <par23mar5@gmail.com>
2019-12-02 18:18:47 +0400

    Added changes

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -3,32 +3,65 @@ function collapse (element) {
     const link = document.getElementsByClassName(element);
     if (!collapse.classList.contains('in')) {
         link[0].innerHTML = "-";
+
         return;
     }
     link[0].innerHTML = "+";
 }
 
+function showFun(){
+        if (document.querySelector('.table').style.display == "none") {
+            document.querySelector('.table').style.display = "inline-table"
+            document.querySelector('.home').style.display = "none"
+        } else {
+            document.querySelector('.table').style.display = "none"
+            document.querySelector('.home').style.display = "block"
+        }
+}
+
+
 // function changeBg(class) {
 //     $('class').css('background-color', 'white');
 // }
-
-
-// function changeBg(block) {
+// let selectedDiv;
+//
+// function changeBg() {
 //     const color = document.getElementsByClassName(block);
 //     block.onclick = function(event) {
 //         let target = event.target; // где был клик?
 //
-//         if (target.tagName = block) return; // не на block? тогда не интересует
+//         if (target.tagName != 'div'){
+//             console.log(111);
+//         } //return; // не на block? тогда не интересует
 //
-//          // подсветить block
+//         // highlight(target);// подсветить block
 //     };
 // };
 //
 //
-// // function color(block){
-// //
-// // }
+// function highlight(a) {
+//     // console.log(111);
+//     if (selectedDiv) { // убрать существующую подсветку, если есть
+//         selectedDiv.classList.remove('highlight');
+//     }
+//     selectedDiv = div;
+//     selectedDiv.classList.add('highlight'); // подсветить новый td
+// }
+
+
+// ;(function($D){
+//     var $a    = $D.querySelector('.block-1');
+//         $container = $D.querySelector('.info');
 //
-
-
-
+//     function changeInfo (e){
+//         let data = e.target.dataset,
+//             toggleText = $a.innerHTML,
+//             isVisible = $container.style.display == 'block';
+//
+//         $a.innerHTML = data.toggleText;
+//         data.toggleText = toggleText;
+//
+//         $container.style.display = isVisible ? 'none' : 'block';
+//     };
+// })(document);
+//
