79e985e827001f002dbcb02127a0f887493b77a8
buckmiller <par23mar5@gmail.com>
2019-12-22 23:05:09 +0400

    Added minor changes

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,3 +1,8 @@
+const phpAdmin = $('#home');
+const newCollapse = $('#new');
+const createNewTable = $('#newTable');
+const contentBlock = $('#info');
+
 function collapse(element) {
     const collapse = document.getElementById(element);
     const link = document.getElementsByClassName(element);
@@ -70,288 +75,24 @@ function showFun(arg) {
         `;
             $(tables).append(tr2);
             $(content).html(tables);
-
-            if (arg === 0) {
-                $(".forChangeCol").css('background', 'linear-gradient(#fff, #dcdcdc)');
-                document.getElementById('info').innerHTML = `
-<div class="home">
-<div class="col-7 col-md-7 information1">
-<div  class="blocks">
-<div><h2>General Settings</h2></div>
-<div class="about">
-<a href="#">Change Password</a>
-<p class="currentServer"><img alt="server" class="img-responsive" src="images/server.png" width="16px" height="16px">Current Server:</p>
-<select name="MySQL">
-<option>MySQL</option>
-<option>.....</option>
-<option>......</option>
-</select>
-</div>
-</div>
-<div class="blocks">
-<div><h2>Appearance Settings</h2></div>
-<div class="about">
-<p class="language"><img alt="server" class="img-responsive" src="images/translation.png" width="15px" height="14px">Language:</p>
-<select name="language" class="languages">
-<option>English</option>
-<option>Russian</option>
-<option>Armenian</option>
-</select>
-<p class="theme"><img alt="server" class="img-responsive" src="images/theme1.png" width="16px" height="16px">Theme:</p>
-<select name="theme" class="themes">
-<option>pmahomme</option>
-<option>....</option>
-<option>.....</option>
-</select>
-</div>
-</div>
-</div>
-<div class="col-5 col-md-5 information2">
-<div class="blocks">
-<div class="background"><h2>MySQL</h2></div>
-<div class="about">
-<p>Font size</p>
-<a href="#">More Settings</a>
-<p>Server connection:<span class="txt">SSL is not being used</span><img src="images/help.png" alt="help" width="21px" height="21px"></p>
-<p>Server version: 8.0.18 - MySQL Community Server - GPL</p>
-<p>Protocol version: 10</p>
-<p>User: root@192.168.30.20</p>
-<p>Server charset: UTF-8 Unicode (utf8mb4)</p>
-</div>
-</div>
-<div class="blocks">
-<div><h2>Web server</h2></div>
-<div class="about">
-<p>nginx/1.2.1</p>
-<p>Database client version:libmysql-5.5.40</p>
-<p>Lorem ipsum Lorem ipsum Lorem ipsum</p>
-<p>PHP extension: mysqli <img src="images/help.png" alt="help" width="21px" height="21px"> curl <img src="images/help.png" alt="help" width="21px" height="21px"> mbstring <img src="images/help.png" alt="help" width="21px" height="21px"></p>
-<p>Lorem ipsumLorem ipsum</p>
-<p>Lorem ipsumLorem ipsum</p>
-<p>Lorem ipsum</p>
-</div>
-</div>
-<div class="blocks" >
-<div><h2>phpMyAdmin</h2></div>
-<div class="about lastBlock">
-<p>Version information</p>
-<p>Git version</p>
-<a>Documentation</a>
-<a>Wiki</a>
-<a>Official Homepage</a>
-<a>Contribute</a>
-<a>Get support</a>
-<a>List of changes</a>
-</div>
-</div>
-</div>
-</div>`
-            }
-            if (arg === 7) {
-                document.querySelector('.forChangeCol').style.background = "white";
-                document.getElementById('info').innerHTML = `
-<div class="mainBlock">
-<h1>Databases</h1>
-<div>
-<img src="images/new.png" width="20px" height="20px" alt="New"><span>Create databases</span><img src="images/help.png"  width="20px" height="20px" alt="Help">
-</div>
-<div class="tbl">
-<input type="text" name="Database" placeholder="Database name" >
-<select>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>.........gfj,.....</option>
-<option>....................</option>
-<option>...........</option>
-<option>ssgsgdgdgg</option>
-<option>utf8mb4_0ps00_ai_ci</option>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>utsg0p00_ai_ci</option>
-<option>utsgsp00_ai_ci</option>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>utf8mg.......ssgsb4_0p00_ai_ci</option>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>utf8mb4_0....p00_ai_ci</option>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>utf8mb4_0p00_ai_ci</option>
-<option>addss</option>
-</select>
-<button onclick="showFun(8)">Create</button>
-</div>
-</div>
-<div class="container-fluid block">
-<div class="blockSpan">
-<div class="container filters"><h4>Filters</h4></div>
-<span>Containing the word <input type="text"></span>
-</div>
-<div class="database">
-<table>
-<tr>
-<th>Database</th>
-<th>Collation</th>
-<th>Master replication</th>
-<th>Action</th>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum </td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum </td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum </td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum </td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum </td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">loum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem idgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem sum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">lorem  lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox"> ipsum lordgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<td><input type="checkbox">dgdgdgem</td>
-<td>lorem ipsum lorem</td>
-<td><img src="images/redx.png" width="20px" height="20px" alt="redx">Not replicated</td>
-<td><img src="images/check.png" width="20px" height="20px" alt="check"><a href="#">Check privileges</a></td>
-</tr>
-<tr>
-<th>Total: 22</th>
-</tr>
-</table>
-<hr>
-</div>
-</div>
-`
-            }
-            if (arg === 8) {
-                document.querySelector('.forChangeCol').style.background = "white";
-                document.getElementById('info').innerHTML = `
-                        <div class="create">
-                            <div class="ct">
-                                <h4><img src="images/new.png" width="20px" height="20px" alt="New"> Create table</h4>
-                            </div>
-                            <div class="name">
-                                <span>Name:<input type="text" required="required"></span>
-                                <span>Number o columns: <input type="number" min="1" value="4" required="required"  style="width: 50px;" ></span>
-                            </div>
-                        </div>
-                        <div class="blue">
-                            <button>Go</button>
-                        </div>
-                        <hr>
-        `
-            }
         }
     });
+}
+
+function staticHtml(param) {
+    switch (param) {
+        case 'home':
+            contentBlock.html(phpAdmin);
+            break;
+        case 'new':
+            newCollapse.removeClass("hidden");
+            contentBlock.html(newCollapse);
+            break;
+        case 'newTable':
+            createNewTable.removeClass("hidden");
+            contentBlock.html(createNewTable);
+            break;
+    }
 }
 
 $.ajax({
